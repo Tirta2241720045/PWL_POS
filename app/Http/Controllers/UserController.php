cfccd00a49cb8e22f028e002cb3bdda6897f879c
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-03-05 13:30:44 +0700

    Praktikum-6 Eloquent Orm

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function showProfile($id, $name)
+    public function index()
     {
-        return view('user.profile', compact('id', 'name'));
+        //tambah data user dengan Eloquent Model
+        $data = [
+            // 'username' => 'customer-1',
+            // 'nama' => 'Pelanggan',
+            'nama' => 'Pelanggan Pertama',
+            // 'password' => Hash::make('12345'),
+            // 'level_id' => 4
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+        
+        //coba akses model UserModel
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
