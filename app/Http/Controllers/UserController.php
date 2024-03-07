561d9a282dcb07fd4987832b0ee7814d64a64d77
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-03-07 17:42:52 +0700

    Praktikum-1 fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,13 +12,12 @@ class UserController extends Controller
     {
         //tambah data user dengan Eloquent Model
         $data = [
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan',
-            'nama' => 'Pelanggan Pertama',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 4
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        UserModel::create($data); //update data user
         
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
