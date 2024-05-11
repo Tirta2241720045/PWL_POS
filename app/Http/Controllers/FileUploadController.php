2384bdc8f75476bf5650db92249f5ae1f56c42cb
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-05-11 21:49:10 +0700

    A.	PERSIAPAN AWAL

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    //
+    public function fileUpload()
+    {
+        return view('file-upload');
+    }
+    public function prosesFileUpload(Request $request)
+    {
+        dump($request->berkas);
+        // dump($request->file('file'));
+        // return "Pemrorsesan file upload di sini";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\FileUploadController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function(){
+    return  view('welcome');
+});
+Route::get('/file-upload', [FileUploadController::class, 'fileUpload']);
+Route::post('/file-upload', [FileUploadController::class, 'prosesFileUpload']);
 
 Route::get('/', [WelcomeController::class, 'index']);
 
