239de2f2f2b050e094253f33831835ceca78c4f9
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-05-12 16:04:24 +0700

    Tugas-1

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,40 +11,25 @@ class FileUploadController extends Controller
     {
         return view('file-upload');
     }
+
     public function prosesFileUpload(Request $request)
     {
         $request->validate([
-            'berkas' => 'required|file|image|max:5000',]);
-            $extfile = $request->berkas->getClientOriginalName();
-            $namaFile = 'web-'.time().".".$extfile;
+            'berkas' => 'required|file|image|max:5000',
+            'nama_file' => 'required|string'
+        ]);
 
-            $path = $request->berkas->move('gambar', $namaFile);
-            $path =str_replace("\\","//", $path);
-            echo "Variabel path berisi: $path <br>";
+        $namaFile = $request->input('nama_file') . '-' . time() . '.' . $request->berkas->getClientOriginalExtension();
 
-            $pathBaru=asset('gambar/'.$namaFile);
-            echo "proses upload berhasil, data disimpan pada:$path";
-            echo "<br>";
-            echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
-                        // $path = $request->berkas->store('uploads');
-            // echo $request->berkas->getClientOriginalName()."lolos validasi";
-        // dump($request->berkas);
-        // dump($request->file('file'));
-        // return "Pemrorsesan file upload di sini";
-        // if($request->hasFile('berkas')){
-        //     echo "path(): ".$request->berkas->path();
-        //     echo "<br>";
-        //     echo "extension(): ".$request->berkas->extension();
-        //     echo "<br>";
-        //     echo "getClientOriginalExtension(): ".
-        //     $request->berkas->getClientOriginalExtension();
-        //     echo "<br>";
-        //     echo "getClientOriginalName(): ".
-        //     $request->berkas->getClientOriginalName();
-        //     echo "<br>";
-        //     echo "getSize(): ".$request->berkas->getSize();
-        // } else {
-        //     echo "Tidak ada file yang diupload";
-        // }
+        $path = $request->berkas->move('gambar', $namaFile);
+        $path = str_replace("\\", "//", $path);
+
+        $pathBaru = asset('gambar/' . $namaFile);
+
+        // Menampilkan pesan sukses dan link gambar
+        echo "Gambar berhasil diupload ke <a href='$pathBaru'>$namaFile</a><br>";
+
+        // Menampilkan gambar yang diunggah
+        echo "<img src='$pathBaru' alt='uploaded_image'>";
     }
 }
