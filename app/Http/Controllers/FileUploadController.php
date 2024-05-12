2b733770fb3dbd271062920a2243cd591348eb62
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-05-12 13:09:18 +0700

    D.	MEMINDAH FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -15,7 +15,12 @@ class FileUploadController extends Controller
     {
         $request->validate([
             'berkas' => 'required|file|image|max:5000',]);
-            echo $request->berkas->getClientOriginalName()."lolos validasi";
+            $extfile = $request->berkas->getClientOriginalName();
+            $namaFile = 'web-'.time().".".$extfile;
+            $path = $request->berkas->storeAs('uploads', $namaFile);
+            echo "proses upload berhasil, file berada di: $path";
+                        // $path = $request->berkas->store('uploads');
+            // echo $request->berkas->getClientOriginalName()."lolos validasi";
         // dump($request->berkas);
         // dump($request->file('file'));
         // return "Pemrorsesan file upload di sini";
