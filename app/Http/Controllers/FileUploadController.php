c9609064ceba305f3505b48d6fd68678c8137af3
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-05-12 12:42:48 +0700

    B.	INFORMASI FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -13,26 +13,26 @@ class FileUploadController extends Controller
     }
     public function prosesFileUpload(Request $request)
     {
-        $request->validate([
-            'berkas' => 'required|file|image|max:5000',]);
-            echo $request->berkas->getClientOriginalName()."lolos validasi";
+        // $request->validate([
+        //     'berkas' => 'required|file|image|max:5000',]);
+        //     echo $request->berkas->getClientOriginalName()."lolos validasi";
         // dump($request->berkas);
         // dump($request->file('file'));
         // return "Pemrorsesan file upload di sini";
-        // if($request->hasFile('berkas')){
-        //     echo "path(): ".$request->berkas->path();
-        //     echo "<br>";
-        //     echo "extension(): ".$request->berkas->extension();
-        //     echo "<br>";
-        //     echo "getClientOriginalExtension(): ".
-        //     $request->berkas->getClientOriginalExtension();
-        //     echo "<br>";
-        //     echo "getClientOriginalName(): ".
-        //     $request->berkas->getClientOriginalName();
-        //     echo "<br>";
-        //     echo "getSize(): ".$request->berkas->getSize();
-        // } else {
-        //     echo "Tidak ada file yang diupload";
-        // }
+        if($request->hasFile('berkas')){
+            echo "path(): ".$request->berkas->path();
+            echo "<br>";
+            echo "extension(): ".$request->berkas->extension();
+            echo "<br>";
+            echo "getClientOriginalExtension(): ".
+            $request->berkas->getClientOriginalExtension();
+            echo "<br>";
+            echo "getClientOriginalName(): ".
+            $request->berkas->getClientOriginalName();
+            echo "<br>";
+            echo "getSize(): ".$request->berkas->getSize();
+        } else {
+            echo "Tidak ada file yang diupload";
+        }
     }
 }
